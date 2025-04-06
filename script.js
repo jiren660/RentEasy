782f428680deead07841b128b0b81409cc1ec020
jiren660 <natividad.kim06@gmail.com>
2025-04-06 23:06:22 +0800

    style lang

---
+++ script.js
@@ -0,0 +1,7 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
